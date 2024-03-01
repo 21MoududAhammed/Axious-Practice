74d54faf52559f9d92f73ab3fbe0ce3e810bb755
21MoududAhammed <21moudud@gmail.com>
2024-03-01 11:36:30 +0600

    axios.get() and error handeling have done

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,95 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import AddPost from "./components/AddPost.jsx";
 import EditPost from "./components/EditPost.jsx";
 import Posts from "./components/Posts";
-import initialPosts from "./data/db.js";
+import axios from "axios";
+// import initialPosts from "./data/db.js";
 
 export default function App() {
-    const [posts, setPosts] = useState(initialPosts);
-    const [post, setPost] = useState(null); // post I am editing
+  const [posts, setPosts] = useState([]);
+  const [post, setPost] = useState(null); // post I am editing
+  const [error, setError] = useState(null);
 
-    const handleAddPost = (newPost) => {
-        const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+  const handleAddPost = (newPost) => {
+    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
 
-        setPosts([
-            ...posts,
-            {
-                id,
-                ...newPost,
-            },
-        ]);
-    };
+    setPosts([
+      ...posts,
+      {
+        id,
+        ...newPost,
+      },
+    ]);
+  };
 
-    const handleDeletePost = (postId) => {
-        if (confirm("Are you sure you want to delete the post?")) {
-            const newPosts = posts.filter((post) => post.id !== postId);
-            setPosts(newPosts);
-        } else {
-            console("You chose not to delete the post!");
-        }
-    };
+  const handleDeletePost = (postId) => {
+    if (confirm("Are you sure you want to delete the post?")) {
+      const newPosts = posts.filter((post) => post.id !== postId);
+      setPosts(newPosts);
+    } else {
+      console("You chose not to delete the post!");
+    }
+  };
 
-    const handleEditPost = (updatedPost) => {
-        const updatedPosts = posts.map((post) =>
-            post.id === updatedPost.id ? updatedPost : post
-        );
-
-        setPosts(updatedPosts);
-    };
-
-    return (
-        <div>
-            <div>
-                <h1>API Request with Axios</h1>
-                <hr />
-
-                <div>
-                    <Posts
-                        posts={posts}
-                        onDeletePost={handleDeletePost}
-                        onEditClick={setPost}
-                    />
-
-                    <hr />
-
-                    {!post ? (
-                        <AddPost onAddPost={handleAddPost} />
-                    ) : (
-                        <EditPost post={post} onEditPost={handleEditPost} />
-                    )}
-                </div>
-            </div>
-        </div>
+  const handleEditPost = (updatedPost) => {
+    const updatedPosts = posts.map((post) =>
+      post.id === updatedPost.id ? updatedPost : post
     );
+
+    setPosts(updatedPosts);
+  };
+
+  // fetch posts by axious
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8000/posts`);
+        if (response && response.data) {
+          setPosts(response.data);
+        }
+      } catch (err) {
+        // if server is not running err dosen't contain response 
+        if (err.response) {
+          setError(
+            `Server Error Status: ${err.status} Message: ${err.message}`
+          );
+        } else {
+          setError(err.message);
+        }
+      }
+    };
+    fetchPosts();
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <h1>API Request with Axios</h1>
+        <hr />
+
+        <div>
+          <Posts
+            posts={posts}
+            onDeletePost={handleDeletePost}
+            onEditClick={setPost}
+          />
+
+          <hr />
+
+          {!post ? (
+            <AddPost onAddPost={handleAddPost} />
+          ) : (
+            <EditPost post={post} onEditPost={handleEditPost} />
+          )}
+          {error && (
+            <>
+              <hr />
+              <div>{error}</div>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
